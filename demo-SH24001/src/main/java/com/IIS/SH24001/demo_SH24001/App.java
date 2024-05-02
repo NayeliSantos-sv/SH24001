7450b5d6115c910866796b9020a5ec80d0621258
nayel <nayel@Soonflower>
2024-05-01 21:54:25 -0600

    SH24001

---
+++ demo-SH24001/src/main/java/com/IIS/SH24001/demo_SH24001/App.java
@@ -0,0 +1,18 @@
+package com.IIS.SH24001.demo_SH24001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw. Hello()  );
+    }
+}
---
+++ demo-SH24001/src/test/java/com/IIS/SH24001/demo_SH24001/AppTest.java
@@ -0,0 +1,20 @@
+package com.IIS.SH24001.demo_SH24001;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
